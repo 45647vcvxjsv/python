416ab67f99b4bf8fa41dcec6da638a01c975fa83
Luis Alvarez <146496575+45647vcvxjsv@users.noreply.github.com>
2024-02-19 21:44:04 -0300

    nuevo

---
+++ Logica/variables.py
@@ -0,0 +1,52 @@
+a = 35
+b = 67
+c = a+b
+
+print (c) 
+
+# Declaración de una variable entera
+numero = 10
+
+# Declaración de una variable de punto flotante
+pi = 3.1416
+
+# Declaración de una variable de cadena (string)
+nombre = "Juan"
+
+# Declaración de una variable booleana
+es_verdadero = True
+
+# Declaración de una variable entera
+numero = 10
+
+print(numero)  # Output: 10
+
+# Cambio de tipo de variable a cadena
+numero = "Diez"
+
+print(numero)  # Output: Diez
+
+numero = 10 
+numero +=1 
+numero += 7
+
+print (numero)
+
+nombre = True
+bienvenida = "hola " + nombre + "como estas?"
+bienvenida = f"hola {nombre} como estas?"
+del bienvenida # este comando te permite borrar la variable 
+
+print(bienvenida)
+
+#operadores de pertenencia (in / not in)
+
+print("yo" in bienvenida)
+print ("cata" not in a bienvenida)
+
+nombreCompleto = "carmen villalobos" # esto es camelcase por que en un principio en vez de un espacio se 
+
+
+
+
+
