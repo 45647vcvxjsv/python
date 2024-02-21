92df83f8ccf4c1f7f11ce5840a497235fce086f7
Luis Alvarez <146496575+45647vcvxjsv@users.noreply.github.com>
2024-02-20 22:04:10 -0300

    dia2

---
+++ Logica/datos-compuestos.py
@@ -0,0 +1,25 @@
+# esto es un array, y la cuenta se empieza desde cero es decir del 0 al 9  
+lista = ["luis alvarez", " jamas ", True, 1.85] 
+print (lista)
+
+#tuplas, son datos que no se pueden modificar 
+tupla = ["luis alvarez", " jamas ", True, 1.85]
+print(tupla)
+
+#creando un conjunto (set), pero no se pude modiifcar ademas no puede haber datos no repetidos 
+# y se puede acceder a travez de un bucle y estan desordenados
+
+conjunto = {"creando las listas con los objetos ", "capaz", 8.98}
+
+#creando un diccionario 
+diccionario = {
+   0: 'nombre' : "capax",
+   1: 'jerez' : "hypnos",
+   2: 'geresi' : "hyonis",
+   3: 'depos' : "5.36",
+   4: 'queriendo' : "gemidos"
+}
+
+print(diccionario['nombre'] + 4)
+print(3)
+
